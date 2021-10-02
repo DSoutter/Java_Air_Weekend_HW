cd6594a6df063608476b246ff91c9266188da26a
DSoutter <duncan.soutter@gmail.com>
2021-10-02 11:15:35 +0100

    cabin crew member has name and rank

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,7 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
+import staff.Employee;
+import staff.Rank;
+import staff.cabinCrewMember.CabinCrewMember;
 
 import static org.junit.Assert.assertEquals;
 
 public class CabinCrewMemberTest {
+
+    private CabinCrewMember cabinCrewMember;
+
+    @Before
+    public void before(){
+        cabinCrewMember = new CabinCrewMember("Dave", Rank.FLIGHTATTENDANT);
+    }
+
+    @Test
+    public void employeeHasName(){
+        assertEquals("Dave", cabinCrewMember.getName());
+    }
+
+    @Test
+    public void employeeHasRank(){
+        assertEquals(Rank.FLIGHTATTENDANT, cabinCrewMember.getRank());
+    }
 }
