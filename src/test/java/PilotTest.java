bebbb046cf6a272e7993468b0ee7d2b13000a3ad
DSoutter <duncan.soutter@gmail.com>
2021-10-02 11:34:23 +0100

    pilot has name rank and license no

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,7 +1,31 @@
 import org.junit.Before;
 import org.junit.Test;
+import staff.Rank;
+import staff.pilot.Pilot;
 
 import static org.junit.Assert.assertEquals;
 
 public class PilotTest {
+
+    private Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Kareem Abdul-Jabbar", Rank.FIRSTOFFICER, "abcd1234");
+    }
+
+    @Test
+    public void pilotHasName(){
+        assertEquals("Kareem Abdul-Jabbar", pilot.getName());
+    }
+
+    @Test
+    public void pilotHasRank(){
+        assertEquals(Rank.FIRSTOFFICER, pilot.getRank());
+    }
+
+    @Test
+    public void pilotHasLicenseNum(){
+        assertEquals("abcd1234", pilot.getLicenseNum());
+    }
 }
